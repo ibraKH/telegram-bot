4668c1a5311767c82104764787fbd314f87d3b3a
ibra <ibrakh80@gamail.com>
2022-02-06 22:29:15 +0300

    solving database

--- database/connection.js
+++ database/connection.js
@@ -12,7 +12,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL || `postgresql://postgres:${process.env.PG_PASSWORD}@localhost:5432/${process.env.PG_DATABASE}`,
-    ssl: process.env.DATABASE_URL ? true : false
+    ssl: { rejectUnauthorized: false }
 })
 
 export default pool;
